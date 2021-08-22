5a3818888abb086423e492580eb3ee75fc0c2275
SJRXD <rohr.olasz@gmail.com>
2021-08-21 22:40:46 -0700

    creating prompts + organizing roles

---
+++ index.js
@@ -0,0 +1,85 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+const Employee = require("./lib/EmployeeClass");
+const EmployeeClass = require("./lib/EmployeeClass");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+var id = 0;
+var teamArray = [];
+
+init = () => {
+    console.log("🌼 TEAM PROFILE GENERATOR 🌼 \nFollow the prompts to create a new employee.");
+    return inquirer.prompt([
+
+    // CREATE EMPLOYEE //
+    {
+        type: "input",
+        name: "eName",
+        message: "What is the name of the new employee?",
+    },
+    {
+        type: "input",
+        name: "eID",
+        message: "Enter the employee's ID.",
+    },
+    {
+        type: "input",
+        name: "eMail",
+        message: "Enter the employee's email address.",
+    },
+    {
+        type: "checkbox",
+        name: "eRole",
+        message: "What is the role of the new employee?",
+        choices: ["Engineer", "Intern", "Manager"],
+    },
+
+    // PUSHING RESULTS TO ARRAY //
+    ]).then((promptResults) => {
+        promptResults.role = "Employee";
+        const { name, id, email } = promptResults;
+        const newEmployee = new Employee (name, id, email);
+        emps.push(newEmployee); 
+    })
+};
+
+// TO BE PROMPTED AFTER ROLE SELECTION? //
+
+// {
+//     type: "input",
+//     name: "mOfficeNumber",
+//     message: "What is Office Number of the new Manager?",
+// },
+// {
+//     type: "input",
+//     name: "iSchool",
+//     message: "Where is the new Intern attending school?",
+// },
+// {
+//     type: "input",
+//     name: "eGitHub",
+//     message: "What is the GitHub username of the new Engineer?",
+// },
+
+// WRITE HTML FILE //
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log("✨ HTML Generated ✨");
+    });
+}
+
+// FUNCTION - INITIALIZE APP //
+// function init() {
+//     inquirer.prompt(createEmp) // PROBABLY WON'T USE
+//     .then(function (userInput) {
+//         console.log(userInput);
+//         writeToFile("./dist/generateHTML.html", generateHTML(userInput));
+//     });
+// };
+
+// INITIALIZE //
+// init();
